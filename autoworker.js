efa52bdcfe306c9b631ac817bd16357e212faad6
Gavin Lloyd <gavinhungry@gmail.com>
2018-09-07 02:28:50 -0700

    Allow for multiple worker methods

--- autoworker.js
+++ autoworker.js
@@ -1,59 +1,96 @@
-class AutoWorker {
-  constructor(fn) {
-    let blob = new Blob([`
-      self.addEventListener('message', async ({ data: { id, args }}) => {
-        let fn = ${fn.toString()};
+/*
+ * autoworker - Web Workers the Easy Way (TM)
+ * https://github.com/gavinhungry/autoworker
+ */
 
+class AutoWorker {
+  /**
+   * Create an AutoWorker instance
+   *
+   * @param {Object | Function} methods - if function, method name is 'run'
+   * @return {AutoWorker}
+   */
+  constructor(methods) {
+    if (typeof methods === 'function') {
+      methods = { run: methods };
+    }
+
+    let names = Object.keys(methods);
+
+    let handler = () => {
+      self.addEventListener('message', async ({ data }) => {
         try {
-          let [ response ] = await Promise.all([fn(...args)]);
-          self.postMessage({ id, response });
+          let fn = methods[data.name];
+
+          // use Promise.all, so our functions can optionally be async
+          let [ result ] = await Promise.all([fn(...data.args)]);
+          self.postMessage({ id: data.id, result });
         } catch(err) {
-          self.postMessage({ id, error: {
+          // we can't send back the entire Error object, so pick properties
+          self.postMessage({ id: data.id, error: {
             message: err.message,
             stack: err.stack
           }
         });
         }
       });
-    `], { type: 'application/javascript' });
+    };
 
-    let url = URL.createObjectURL(blob);
-    this.worker = new Worker(url);
+    let blob = new Blob([
+      `let methods = {};`,
+      ...names.map(name => `methods['${name}'] = ${methods[name]};`),
+
+      `(${handler})();`
+    ], { type: 'application/javascript' });
+
+    this.url = URL.createObjectURL(blob);
+    this.worker = new Worker(this.url);
+
+    // add helper instance methods for each named worker method
+    names.forEach(name => {
+      this[name] = (...args) => this.exec(name, ...args);
+    });
   }
 
   /**
    * Get a unique ID string
    *
-   * @param {Number} len - output string length
+   * @param {Number} length - output string length
    * @return {String}
    */
-  static generateId(len = 16) {
-    return Array.from(crypto.getRandomValues(new Uint8Array(len)))
+  static generateId(length = 16) {
+    return Array.from(crypto.getRandomValues(new Uint8Array(length)))
       .map(v => String.fromCharCode((Math.floor(v % 26) + 97))).join('');
   }
 
-  run(...args) {
+  /**
+   * Get the results of a named method executed in the worker thread
+   *
+   * @param {String} name - method name
+   * @param {...Mixed} args - method arguments
+   * @return {Promise} method results
+   */
+  exec(name, ...args) {
     return new Promise((resolve, reject) => {
-      let messageId = AutoWorker.generateId();
+      let id = AutoWorker.generateId();
 
-      this.worker.addEventListener('message', ({ data: {
-        id, error, response
-      }}) => {
-        if (id !== messageId) {
+      this.worker.addEventListener('message', ({ data }) => {
+        // this result is not for us
+        if (data.id !== id) {
           return;
         }
 
-        if (error) {
-          let err = new Error(error.message);
-          err.stack = error.stack;
+        if (data.error) {
+          let err = new Error(data.error.message);
+          err.stack = data.error.stack;
 
           return reject(err);
         }
 
-        resolve(response);
+        resolve(data.result);
       });
 
-      this.worker.postMessage({ id: messageId, args });
+      this.worker.postMessage({ id, name, args });
     });
   }
 }
