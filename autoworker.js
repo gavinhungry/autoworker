eb2d8980448876d7df4ed5ed67699e5bd75e6235
Gavin Lloyd <gavinhungry@gmail.com>
2018-09-07 01:24:08 -0700

    Add initial implementation

---
+++ autoworker.js
@@ -0,0 +1,61 @@
+class AutoWorker {
+  constructor(fn) {
+    let blob = new Blob([`
+      self.addEventListener('message', async ({ data: { id, input }}) => {
+        let fn = ${fn.toString()};
+
+        try {
+          let [ response ] = await Promise.all([fn(input)]);
+          self.postMessage({ id, response });
+        } catch(err) {
+          self.postMessage({ id, error: {
+            message: err.message,
+            stack: err.stack
+          }
+        });
+        }
+      });
+    `], { type: 'application/javascript' });
+
+    let url = URL.createObjectURL(blob);
+    this.worker = new Worker(url);
+  }
+
+  /**
+   * Get a unique ID string
+   *
+   * @param {Number} len - output string length
+   * @return {String}
+   */
+  static generateId(len = 16) {
+    return Array.from(crypto.getRandomValues(new Uint8Array(len)))
+      .map(v => String.fromCharCode((Math.floor(v % 26) + 97))).join('');
+  }
+
+  run(input) {
+    return new Promise((resolve, reject) => {
+      let messageId = AutoWorker.generateId();
+
+      this.worker.addEventListener('message', ({ data: {
+        id, error, response
+      }}) => {
+        if (id !== messageId) {
+          return;
+        }
+
+        if (error) {
+          let err = new Error(error.message);
+          err.stack = error.stack;
+
+          return reject(err);
+        }
+
+        resolve(response);
+      });
+
+      this.worker.postMessage({ id: messageId, input });
+    });
+  }
+}
+
+module.exports = AutoWorker;
